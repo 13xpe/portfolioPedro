f33d24c0ccec1b509d7dd6fd7bee89b197cfd87e
Pedro <124820216+13xpe@users.noreply.github.com>
2024-01-24 14:03:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
 
-function App() {
+export default function App() {
   return (
     <div>
       <NavBar />
@@ -17,5 +17,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
